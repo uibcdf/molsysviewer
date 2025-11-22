2d7dd37f8ff90dba84fa70cb8527076cd248415c
Diego Prada <prada.gracia@gmail.com>
2025-11-22 14:52:25 -0600

    In process

--- molsysviewer/widget.py
+++ molsysviewer/widget.py
@@ -2,4 +2,4 @@ import anywidget
 from pathlib import Path
 
 class MolSysViewerWidget(anywidget.AnyWidget):
-    _esm = (Path(__file__).parent / "viewer.js").read_text()
+    _esm = (Path(__file__).parent / "viewer.js").read_text(encoding="utf-8")
